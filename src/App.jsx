518a820153e389b50c9fdc477e42bb1e7becd158
ketanchhajed07 <ketanchhajed07@gmail.com>
2024-08-07 13:48:12 +0530

    pages created and implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
-import { css } from "../styled-system/css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Links from "./pages/Links";
+import Profile from "./pages/Profile";
+import Preview from "./pages/Preview";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Navigate to="/login" /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  {
+    path: "/create",
+    children: [
+      { path: "links", element: <Links />, index: true },
+      { path: "profile", element: <Profile /> },
+    ],
+  },
+
+  { path: "/preview", element: <Preview /> },
+]);
+
 function App() {
-  return <div className={css({ color: "red" })}>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
